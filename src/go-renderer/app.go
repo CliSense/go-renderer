e5b382fc72778e7d44959d4546804526ef89cb19
Devang Tomar <32426600+devangtomar@users.noreply.github.com>
2023-09-24 15:13:20 +0530

    Adding renderer code from chatgpt

---
+++ src/go-renderer/app.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/rivo/tview"
+	"time"
+)
+
+func main() {
+	app := tview.NewApplication()
+
+	// Create a TextView widget for the loading animation
+	loadingText := tview.NewTextView().
+		SetText("Loading...").
+		SetTextAlign(tview.AlignCenter).
+		SetTextAlignVertical(tview.AlignMiddle).
+		SetDynamicColors(true).
+		SetText("")
+
+	// Add the TextView to the application
+	if err := app.SetRoot(loadingText, true).Run(); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			for i := 0; i < 4; i++ {
+				// Clear the TextView
+				loadingText.Clear()
+
+				// Set the loading animation text
+				loadingText.SetText("Loading" + "...."[i:i+1])
+
+				// Refresh the application
+				app.Draw()
+
+				// Sleep for a short duration to control animation speed
+				time.Sleep(250 * time.Millisecond)
+			}
+		}
+	}()
+
+	// Simulate some work here
+	time.Sleep(5 * time.Second)
+
+	// Close the application
+	app.Stop()
+}
